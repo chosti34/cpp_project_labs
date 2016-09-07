2d6b5502b1834830fa5f2efcb02dc928a3ea29e9
timur <chosti34@gmail.com>
2016-09-07 23:06:26 +0300

    Изменены названия переменных, улучшена читаемость кода

--- labwork_01_01/jump_height.cpp
+++ labwork_01_01/jump_height.cpp
@@ -9,47 +9,48 @@
 // see also https://ps-group.github.io/sfml/coding_conventions.html
 // TODO: fix negative height values, fix heigh values higher than max height.
 
-int main(int, char *[])
+int main()
 {
     const float g = 9.8f;
-    float T;
-    float Vn;
-    int S;
-    int M;
-    printf("S: ");
-    if (0 == scanf("%d", &S))
+
+    float maxHeight, timeOfMaxHeight;
+
+    printf("Enter jump height, please: ");
+
+    if (scanf("%f", &maxHeight) == 0)
     {
-        printf("\n" "expected floating-point number" "\n");
+        printf("Expected floating-point or integer number\n");
         exit(1);
     }
+
     // T - time point when height is at maximum.
     // t - current time point
     // v(t) == v0 - g * t
     // v0 = g * T
     // s(t) == v0 * t - 0.5 * g * t * t
-    T = sqrt(S * 2 / g);
-    printf("T=%f\n", T);
+
+    timeOfMaxHeight = sqrt(maxHeight * 2 / g);
+    printf("Time when height has it's maximum value = %f\n", timeOfMaxHeight);
+
     bool flag = false;
-    for (float t = 0; t < T * 2; t += 0.1f)
+    for (float time = 0; time < timeOfMaxHeight * 2; time += 0.1f)
     {
-        if (t > T && !flag)
+        if (time > timeOfMaxHeight && !flag)
         {
             flag = true;
-            float V0 = g * T;
-            float s = V0 * T - 0.5 * g * T * T;
-            printf("t=%f, s=%f\n", T, s);
+            float startingSpeed = g * timeOfMaxHeight;
+            float currentHeight = (startingSpeed * timeOfMaxHeight) - (0.5f * g * timeOfMaxHeight * timeOfMaxHeight);
+            printf("time = %f, height = %f\n", timeOfMaxHeight, currentHeight);
         }
-        float V0 = g * T;
-        float s = V0 * t - 0.5 * g * t * t;
-        printf("t=%f, s=%f\n", t, s);
+
+        float startingSpeed = g * timeOfMaxHeight;
+        float currentHeight = (startingSpeed * time) - (0.5f * g * time * time);
+        printf("time = %f, height = %f\n", time, currentHeight);
     }
 
-    float V0 = g * T;
-    float s = V0 * (T*2) - 0.5 * g * (T * 2) * (T * 2);
-    printf("t=%f, s=%f\n", T * 2, s);
-
-    // TODO: remove system("pause") and never use it again.
-    system("pause");
+    float startingSpeed = g * timeOfMaxHeight;
+    float currentHeight = (startingSpeed * (2 * timeOfMaxHeight)) - 0.5f * g * (timeOfMaxHeight * 2) * (timeOfMaxHeight * 2);
+    printf("time = %f, height = %f\n", 2 * timeOfMaxHeight, currentHeight);
 
     return 0;
 }

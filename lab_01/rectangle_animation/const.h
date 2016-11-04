51a9ef2635540e4df6cde81f4a81c6d297ea03b1
Timur Karimov <chosti34@gmail.com>
2016-11-04 17:18:04 +0300

    Добавлен вывод массива прямоугольников на экран

---
+++ lab_01/rectangle_animation/const.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "stdafx.h"
+
+// начальный цвет прямоугольников
+const sf::Color START_COLOR = sf::Color(0, 0, 100, 255);
+
+// начальный размер прямоугольников
+const float RECTANGLE_WIDTH = 50;
+const float RECTANGLE_HEIGHT = 50;
+
+// расстояние между прямоугольниками (всегда постоянно)
+const float RECTANGLE_DISTANCE = 10;
--- lab_01/rectangle_animation/main.cpp
+++ lab_01/rectangle_animation/main.cpp
@@ -2,37 +2,46 @@
 //
 
 #include "stdafx.h"
+#include "const.h"
+
+void InitializeRectangles(sf::RectangleShape rectangles[5])
+{
+    for (size_t i = 0; i < 5; ++i)
+    {
+        rectangles[i].setFillColor(START_COLOR);
+        rectangles[i].setSize(sf::Vector2f(RECTANGLE_WIDTH, RECTANGLE_HEIGHT));
+        rectangles[i].setOrigin(RECTANGLE_WIDTH / 2, RECTANGLE_HEIGHT / 2);
+        rectangles[i].setPosition(10 + i * (RECTANGLE_WIDTH + RECTANGLE_DISTANCE) + rectangles[i].getOrigin().x, 10 + rectangles[i].getOrigin().y);
+    }
+}
+
+void DrawRectangles(sf::RenderWindow &window, sf::RectangleShape rectangles[5])
+{
+    for (size_t i = 0; i < 5; ++i)
+    {
+        window.draw(rectangles[i]);
+    }
+}
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "Rectangle Animation");
 
-    sf::CircleShape shape(100.f);
-
-    shape.setFillColor(sf::Color::Green);
+    sf::RectangleShape rectangles[5];
+    InitializeRectangles(rectangles);
 
     while (window.isOpen())
-
     {
-
         sf::Event event;
-
         while (window.pollEvent(event))
-
         {
-
             if (event.type == sf::Event::Closed)
-
                 window.close();
-
         }
 
         window.clear();
-
-        window.draw(shape);
-
+        DrawRectangles(window, rectangles);
         window.display();
-
     }
 
     return 0;
